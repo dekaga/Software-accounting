48a61b6fad4dc525e3d025386c2d8ab1503a0a06
у <@56_102>
2024-06-07 10:28:21 +0900

    Добавлены комментарии к странице добавления нового компьютера

--- WPF APP/AddEditPage.xaml.cs
+++ WPF APP/AddEditPage.xaml.cs
@@ -18,16 +18,18 @@ namespace WPF_APP
 
     public partial class AddEditPage : Page
     {
-        private Компьютер _currentHotel = new Компьютер();
+        private Компьютер _currentHotel = new Компьютер(); // Поле для отслеживания текущего редактируемого компьюьтера
         private Компьютер компьютер;
 
         public AddEditPage(Компьютер selectedHotel)
         {
             InitializeComponent();
+            //// Если передан копмпьютер для редактирования, то скопируем его в поле для отслеживания текущего редактируемого компьютера
             if (selectedHotel != null)
             {
                 _currentHotel = selectedHotel;
             }
+            // Устанавливаем контекст данных для формы
             DataContext = _currentHotel;
         }
         private void BtnAdd_Click(object sender, RoutedEventArgs e)
@@ -36,22 +38,27 @@ namespace WPF_APP
         }
         private void BtnSave_Click(object sender, RoutedEventArgs e)
         {
+            // Создаем строку для вывода ошибок
             StringBuilder errors = new StringBuilder();
 
             if (string.IsNullOrWhiteSpace(_currentHotel.номер_компьютера))errors.AppendLine("Укажите номер компьютера");
+            // Если есть ошибки, то выводим их и возвращаемся
             if (errors.Length > 0)
             {
                 MessageBox.Show(errors.ToString());
                 return;
             }
+            // Если текущий компьютер имеет id=0, то это новый компьютер
             if (_currentHotel.id == 0)
                 Krivosheev_zadanieEntities1.GetContext().Компьютер.Add(_currentHotel);
+            // Cохраняем изменения в БД
             try
             {
                 Krivosheev_zadanieEntities1.GetContext().SaveChanges();
                 MessageBox.Show("Информация сохранена");
             }
-            catch(Exception ex)
+            // Если произошла ошибка при сохранении, то выводим сообщение об ошибке
+            catch (Exception ex)
             {
                 MessageBox.Show(ex.Message.ToString());
             }
