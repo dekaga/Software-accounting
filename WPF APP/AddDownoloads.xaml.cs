ac26ef87f3e7be77a8daeffcfbf9243c6e515054
у <@56_102>
2024-06-07 10:25:24 +0900

    Добавлены комментарии к окну AddDownoloads.xaml.cs

--- WPF APP/AddDownoloads.xaml.cs
+++ WPF APP/AddDownoloads.xaml.cs
@@ -20,35 +20,43 @@ namespace WPF_APP
     /// </summary>
     public partial class AddDownoloads : Page
     {
-        private Установка _currentDownoloads = new Установка();
+        private Установка _currentDownoloads = new Установка(); // Поле для отслеживания текущей редактируемой установки
         private Установка Установка;
-        public AddDownoloads(Установка selectedDownoloads)
+        public AddDownoloads(Установка selectedDownoloads) 
         {
             InitializeComponent();
+            //// Если передана установка для редактирования, то скопируем ее в поле для отслеживания текущей редактируемой установки
+
             if (selectedDownoloads != null)
             {
                 _currentDownoloads = selectedDownoloads;
             }
+            // Устанавливаем контекст данных для формы
             DataContext = _currentDownoloads;
         }
 
-        private void BtnSave_Click(object sender, RoutedEventArgs e)
+        private void BtnSave_Click(object sender, RoutedEventArgs e) //
         {
+            // Создаем строку для вывода ошибок
             StringBuilder errors = new StringBuilder();
-
+            // Проверяем обязательные поля
             if (string.IsNullOrWhiteSpace(_currentDownoloads.режим_установки)) errors.AppendLine("Укажите режим установки");
+            // Если есть ошибки, то выводим их и возвращаемся
             if (errors.Length > 0)
             {
                 MessageBox.Show(errors.ToString());
                 return;
             }
+            // Если текущая установка имеет id=0, то это новая установка
             if (_currentDownoloads.id == 0)
                 Krivosheev_zadanieEntities1.GetContext().Установка.Add(_currentDownoloads);
+            // Cохраняем изменения в БД
             try
             {
                 Krivosheev_zadanieEntities1.GetContext().SaveChanges();
                 MessageBox.Show("Информация сохранена");
             }
+            // Если произошла ошибка при сохранении, то выводим сообщение об ошибке
             catch (Exception ex)
             {
                 MessageBox.Show(ex.Message.ToString());
